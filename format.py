6c13e13f6817f134a46f7727dfb4981b42b12692
tasi788 <tasi788@gmail.com>
2017-01-22 23:46:23 +0800

    thumbnail ver.

--- format.py
+++ format.py
@@ -8,28 +8,11 @@ pnglist = glob( "*.[pP][nN][gG]" )
 num = 0
 for jpg in jpglist:
 	im = Image.open(jpg)
-	ori_w , ori_h = im.size
 	num += 1
 	png = str(num) + '.png'
-
-	if ori_w + ori_h <= 1024:
-		if ori_w < ori_h:
-			new = 512/float(ori_h)
-			im.resize((ori_h*new,ori_w*new)).save(png)
-		else:
-			new = 512/float(ori_w)
-			im.resize((ori_h*new,ori_w*new)).save(png)
-	else:
-		if ori_w > ori_h:
-			new = ori_w/float(512)
-			print str(new)+'new1'
-			im.resize((ori_h*new,ori_w*new)).save(png)
-		else:
-			new = ori_h/float(512)
-			print str(new)
-			im.resize((ori_h/new,ori_w/new)).save(png)
-			#png = splitext(jpg)[0]+".png"
-			print png
+	im.thumbnail( (512,512) )
+	im.save(png)
+	print png
 
 for png in pnglist:
 	if 	os.path.getsize(png) > 350000:
