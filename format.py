a877dc3c1bf2530a2c1c53b01e98ea467c8c7d6b
tasi788 <tasi788@gmail.com>
2017-01-22 23:39:07 +0800

    float problem

--- format.py
+++ format.py
@@ -9,22 +9,27 @@ num = 0
 for jpg in jpglist:
 	im = Image.open(jpg)
 	ori_w , ori_h = im.size
-	print str(ori_w)+','+str(ori_h)
-	if ori_w + ori_h <= 1024:
-		if ori_w < ori_h:
-			new_w = 512/ori_h
-		else:
-			new_h = 512/ori_w
-	else:
-		if ori_w > ori_h:
-			new_w = ori_w/512
-		else:
-			new_h = ori_h/512
 	num += 1
 	png = str(num) + '.png'
-	#png = splitext(jpg)[0]+".png"
-	im.save(png)
-	print png
+
+	if ori_w + ori_h <= 1024:
+		if ori_w < ori_h:
+			new = 512/float(ori_h)
+			im.resize((ori_h*new,ori_w*new)).save(png)
+		else:
+			new = 512/float(ori_w)
+			im.resize((ori_h*new,ori_w*new)).save(png)
+	else:
+		if ori_w > ori_h:
+			new = ori_w/float(512)
+			print str(new)+'new1'
+			im.resize((ori_h*new,ori_w*new)).save(png)
+		else:
+			new = ori_h/float(512)
+			print str(new)
+			im.resize((ori_h/new,ori_w/new)).save(png)
+			#png = splitext(jpg)[0]+".png"
+			print png
 
 for png in pnglist:
 	if 	os.path.getsize(png) > 350000:
