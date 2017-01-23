25e28fe739226236d9f231a679a75069618b039b
tasi788 <tasi788@gmail.com>
2017-01-23 18:20:06 +0800

    save point!

--- format.py
+++ format.py
@@ -10,9 +10,31 @@ os.system('cd stickers@2x && rm *_key@2x.png *.meta tab*')
 for png in pnglist:
 	im = Image.open(png)
 	png = splitext(png)[0]+".png"
-	im.thumbnail( (512,512) )
-	im.save(png)
-	print png
+	ori_w , ori_h = im.size
+	if ori_w + ori_h <= 1024:
+		if ori_w < ori_h:
+			new = 512/float(ori_h)
+			new_w = new*ori_w
+			new_h = new*ori_h
+			im.resize((new_w,new_h)).save(png)
+		else:
+			new = 512/float(ori_w)
+			new_w = new*ori_w
+			new_h = new*ori_h
+			im.resize((new_w,new_h)).save(png)
+	else:
+		if ori_w > ori_h:
+			new = ori_w/float(512)
+			new_w = ori_w/new
+			new_h = ori_h/new
+			im.resize((new_w,new_h)).save(png)
+		else:
+			new = ori_h/float(512)
+			new_w = ori_w/new
+			new_h = ori_h/new
+			im.resize((new_w,new_h)).save(png)
+			#png = splitext(jpg)[0]+".png"
+			print png
 	if 	os.path.getsize(png) > 350000:
 		im = Image.open(png)
 		im.thumbnail( (512,512) )
