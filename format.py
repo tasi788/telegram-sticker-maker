e6f46a9cdaff560e6d9ff58f54b764c7986367fe
tasi788 <tasi788@gmail.com>
2017-01-23 18:26:56 +0800

    work fine!

--- format.py
+++ format.py
@@ -4,18 +4,7 @@ from os.path import splitext, getsize
 from PIL import Image
 
 #jpglist = glob( "stickers@2x/*.[jJ][pP][gG]" )
-pnglist = glob( "*.[pP][nN][gG]" )
-<<<<<<< HEAD
-=======
-num = 0
-for jpg in jpglist:
-	im = Image.open(jpg)
-	num += 1
-	png = str(num) + '.png'
-	im.thumbnail( (512,512) )
-	im.save(png)
-	print png
->>>>>>> parent of 5ff5725... fine ver
+pnglist = glob( "stickers@2x/*.[pP][nN][gG]" )
 
 os.system('cd stickers@2x && rm *_key@2x.png *.meta tab*')
 for png in pnglist:
@@ -25,34 +14,30 @@ for png in pnglist:
 	if ori_w + ori_h <= 1024:
 		if ori_w < ori_h:
 			new = 512/float(ori_h)
-			new_w = new*ori_w
-			new_h = new*ori_h
+			new_w = int(new*ori_w+0.5)
+			new_h = int(new*ori_h+0.5)
 			im.resize((new_w,new_h)).save(png)
 		else:
 			new = 512/float(ori_w)
-			new_w = new*ori_w
-			new_h = new*ori_h
+			new_w = int(new*ori_w+0.5)
+			new_h = int(new*ori_h+0.5)
 			im.resize((new_w,new_h)).save(png)
 	else:
 		if ori_w > ori_h:
 			new = ori_w/float(512)
-			new_w = ori_w/new
-			new_h = ori_h/new
+			new_w = int(ori_w/new+0.5)
+			new_h = int(ori_h/new+0.5)
 			im.resize((new_w,new_h)).save(png)
 		else:
 			new = ori_h/float(512)
-			new_w = ori_w/new
-			new_h = ori_h/new
+			new_w = int(ori_w/new+0.5)
+			new_h = int(ori_h/new+0.5)
 			im.resize((new_w,new_h)).save(png)
 			#png = splitext(jpg)[0]+".png"
 			print png
 	if 	os.path.getsize(png) > 350000:
 		im = Image.open(png)
-<<<<<<< HEAD
-		im.thumbnail( (512,512) )
 		im.save(jpg, quality=80)
 		im = Image.open(jpg)
 		im.save(png)
 		print 'big except'
-=======
->>>>>>> parent of 5ff5725... fine ver
