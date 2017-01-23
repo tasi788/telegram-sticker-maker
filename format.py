ea539287f23a55a6fbfa0eb3c3fc0bc4076b2876
tasi788 <tasi788@gmail.com>
2017-01-23 18:52:19 +0800

    solve resize problem
    reszie use Image.ANTIALIAS

--- format.py
+++ format.py
@@ -9,37 +9,40 @@ pnglist = glob( "stickers@2x/*.[pP][nN][gG]" )
 for png in pnglist:
 	im = Image.open(png)
 	png = splitext(png)[0]+".png"
+	#resize((512,443),Image.ANTIALIAS).save('new.png')
 	ori_w , ori_h = im.size
 	if ori_w + ori_h <= 1024:
 		if ori_w < ori_h:
 			new = 512/float(ori_h)
 			new_w = int(new*ori_w+0.5)
 			new_h = int(new*ori_h+0.5)
-			im.resize((new_w,new_h)).save(png)
+			im.convert('RGBA').resize((new_w,new_h),Image.ANTIALIAS).save(png)
 			print png
 		else:
 			new = 512/float(ori_w)
 			new_w = int(new*ori_w+0.5)
 			new_h = int(new*ori_h+0.5)
-			im.resize((new_w,new_h)).save(png)
+			im.convert('RGBA').resize((new_w,new_h),Image.ANTIALIAS).save(png)
 			print png
 	else:
 		if ori_w > ori_h:
 			new = ori_w/float(512)
 			new_w = int(ori_w/new+0.5)
 			new_h = int(ori_h/new+0.5)
-			im.resize((new_w,new_h)).save(png)
+			im.convert('RGBA').resize((new_w,new_h),Image.ANTIALIAS).save(png)
 			print png
 		else:
 			new = ori_h/float(512)
 			new_w = int(ori_w/new+0.5)
 			new_h = int(ori_h/new+0.5)
-			im.resize((new_w,new_h)).save(png)
+			im.convert('RGBA').resize((new_w,new_h),Image.ANTIALIAS).save(png)
 			#png = splitext(jpg)[0]+".png"
 			print png
 	if 	os.path.getsize(png) > 350000:
+		jpg = splitext(png)[0]+".jpg"
 		im = Image.open(png)
 		im.save(jpg, quality=80)
 		im = Image.open(jpg)
 		im.save(png)
+		os.system('rm *.jpg')
 		print 'big except'
